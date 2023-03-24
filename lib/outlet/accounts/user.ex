230181a56bc866e4c6e026da67b45db1b2f7fb39
charlieroth <charlieroth4@icloud.com>
2023-03-24 13:51:44 +0100

    update users table to use uuid primary key

--- lib/outlet/accounts/user.ex
+++ lib/outlet/accounts/user.ex
@@ -1,4 +1,17 @@
 defmodule Outlet.Accounts.User do
-  @derive Jason.Encoder
-  defstruct [:id, :name, :age]
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+
+  schema "users" do
+    field(:name, :string)
+    field(:age, :integer)
+  end
+
+  def changeset(user, attrs \\ %{}) do
+    user
+    |> cast(attrs, [:name, :age])
+    |> validate_required([:name, :age])
+  end
 end
